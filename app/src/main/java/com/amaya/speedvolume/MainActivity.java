d80bbe87c3e36e42b7c8448dce55fa70b48c374d
Jose Antonio <jamaya@bubocar.com>
2019-08-31 01:07:25 +0200

    - Stable version: music level changes according to speed input levels   - Update UI   - Added music volume manager

--- app/src/main/java/com/amaya/speedvolume/MainActivity.java
+++ app/src/main/java/com/amaya/speedvolume/MainActivity.java
@@ -1,16 +1,15 @@
 package com.amaya.speedvolume;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Looper;
+import android.media.AudioManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,18 +19,32 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     // Global properties
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
     public static final String TAG = "MainActivity";
 
-    private Activity globalActivity;
-    private Context globalContext;
-
-    private boolean isActivated;
     LocationListener locationListener;
     LocationManager locationManager;
+    AudioManager audioManager;
+
+    DecimalFormat df;
+    DecimalFormat dfAccuracy;
+
+    Integer speed_level_1 = 0;
+    Integer speed_level_2 = 0;
+    Integer speed_level_3 = 0;
+    Integer speed_level_4 = 0;
+    Integer speed_level_5 = 0;
+
+    Double volume_level_1 = 0.2;
+    Double volume_level_2 = 0.3;
+    Double volume_level_3 = 0.5;
+    Double volume_level_4 = 0.7;
+    Double volume_level_5 = 0.85;
     // ---
 
     // UI
@@ -42,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText et_85;
     private EditText et_100;
     private TextView tv_speed;
+    private TextView tv_accuracy_speed;
     // ---
 
     @Override
@@ -56,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // Functions
     private void setUI() {
         switch_activate = (Switch) this.findViewById(R.id.switch_activate);
         et_30 = (EditText) this.findViewById(R.id.et_30);
@@ -64,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
         et_85 = (EditText) this.findViewById(R.id.et_85);
         et_100 = (EditText) this.findViewById(R.id.et_100);
         tv_speed = (TextView) this.findViewById(R.id.tv_speed);
+        tv_accuracy_speed = (TextView) this.findViewById(R.id.tv_accuracy_speed);
 
         switch_activate.setChecked(false);
     }
@@ -74,13 +90,15 @@ public class MainActivity extends AppCompatActivity {
 
     private void initializeActivity() {
 
-
-            // Acquire a reference to the system Location Manager
-            locationManager = (LocationManager) this
-                    .getSystemService(Context.LOCATION_SERVICE);
-
+            // Location manager
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            // Location listener
             locationListener = new LocationListener();
 
+            audioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+            // Utils
+            df = new DecimalFormat("#");
+            dfAccuracy = new DecimalFormat("#.##");
     }
 
     private void startLocationRequestUpdates() {
@@ -106,32 +124,102 @@ public class MainActivity extends AppCompatActivity {
                 MY_PERMISSIONS_REQUEST_LOCATION);
     }
 
+
+    private void setSpeedText(Location location) {
+        if (location != null) {
+            String speed = df.format(location.getSpeed()/3.6);
+            String accuracySpeed = dfAccuracy.format(location.getSpeedAccuracyMetersPerSecond()/3.6);
+            tv_speed.setText(speed);
+            tv_accuracy_speed.setText(accuracySpeed);
+        } else {
+            tv_speed.setText("0");
+            tv_accuracy_speed.setText("0.00");
+        }
+    }
+
+
+    private void setVolumeLevel(Location location) {
+        Log.i(TAG, "setVolumeLevel: hola");
+
+        int speed = Math.round(location.getSpeed());
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int newVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        if (speed <= speed_level_1) {
+            newVolume = (int) (maxVolume * volume_level_1);
+        } else if (speed > speed_level_1 && speed <= speed_level_2) {
+            newVolume = (int) (maxVolume * volume_level_2);
+        } else if (speed > speed_level_2 && speed <= speed_level_3) {
+            newVolume = (int) (maxVolume * volume_level_3);
+        } else if (speed > speed_level_3 && speed <= speed_level_4) {
+            newVolume = (int) (maxVolume * volume_level_4);
+        } else if (speed > speed_level_4 && speed <= speed_level_5) {
+            newVolume = (int) (maxVolume * volume_level_5);
+        } else if (speed > speed_level_5) {
+            newVolume = maxVolume;
+        }
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, newVolume, 0);
+    }
+
+    private void setSpeedLevels() {
+        // que pasa si getText está sin rellenar
+        speed_level_1 = Integer.valueOf(et_30.getText().toString());
+        speed_level_2 = Integer.valueOf(et_50.getText().toString());
+        speed_level_3 = Integer.valueOf(et_70.getText().toString());
+        speed_level_4 = Integer.valueOf(et_85.getText().toString());
+        speed_level_5 = Integer.valueOf(et_100.getText().toString());
+    }
+
+
+    private void showDialog() {
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Los campos están vacíos");
+        alertDialog.setMessage("Debe rellenar los niveles de velocidad");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+    // ---
+
     // Listeners
     private class OnCheckedChangeListener implements CompoundButton.OnCheckedChangeListener {
 
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            isActivated = isChecked;
 
-            if (isChecked) {
-                // Activamos el servicio
-                startLocationRequestUpdates();
+            if (et_30.getText().length() > 0
+                    && et_50.getText().length() > 0
+                    && et_70.getText().length() > 0
+                    && et_85.getText().length() > 0
+                    && et_100.getText().length() > 0) {
+                if (isChecked) {
+                    // Activamos el servicio
+                    startLocationRequestUpdates();
+                    setSpeedLevels();
+                } else {
+                    // Desactivamos el servicio
+                    stopLocationRequestUpdates();
+                    setSpeedText(null);
+                }
             } else {
-                // Desactivamos el servicio
-                stopLocationRequestUpdates();
+                showDialog();
+                switch_activate.setChecked(false);
             }
+
         }
     }
+
 
     private class LocationListener implements android.location.LocationListener {
 
         @Override
         public void onLocationChanged(Location location) {
-
-            String speed =  String.format("%.02f", location.getSpeedAccuracyMetersPerSecond()/3.6);
-            tv_speed.setText(speed + " km/h");
-
-            Log.i(TAG, "onLocationChanged: speed: " + location.getSpeed());
+            setVolumeLevel(location);
+            setSpeedText(location);
         }
 
         @Override
@@ -152,7 +240,6 @@ public class MainActivity extends AppCompatActivity {
     // ---
 
     // Override
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
@@ -160,7 +247,7 @@ public class MainActivity extends AppCompatActivity {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    initializeActivity();
+                    startLocationRequestUpdates();
                 } else {
                     Toast.makeText(this, "Es necesario aceptar los permisos", Toast.LENGTH_LONG).show();
                 }
@@ -170,6 +257,22 @@ public class MainActivity extends AppCompatActivity {
             default:
                 super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        locationManager.removeUpdates(locationListener);
     }
 
     // ---
